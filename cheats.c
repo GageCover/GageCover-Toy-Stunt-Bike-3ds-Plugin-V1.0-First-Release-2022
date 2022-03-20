167774b137f400b1944a9b37196d4c9cba52f471
KingCraXy <cobergage@gmail.com>
2022-03-20 01:08:04 -0500

    Add files via upload

---
+++ cheats.c
@@ -0,0 +1,24 @@
+﻿void	~#ZeroOneZeroOneAZero~Gage_C_Toy_Stunt_Bike_Plugin(void)
+{
+	offset = 0;
+	data = 0;
+}
+
+void	Score_Hack(void)
+{
+	offset = 0x00000000;
+	WRITEU32(offset + 0x02FA590, 0x0098967F);
+	offset = 0;
+	data = 0;
+}
+
+void	Imediatly_End_Match(void)
+{
+	if (is_pressed(0x00000201))
+	{
+		offset = 0x00000000;
+		WRITEU32(offset + 0x02FA598, 0x00000003);
+		offset = 0;
+		data = 0;
+	}
+}
